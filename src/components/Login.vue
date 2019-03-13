dcf70a5961160221a5d00c083d8bdb410bf5b016
François D <fdex24@gmail.com>
2019-03-13 13:58:36 +0100

    Update login layout

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,47 +1,49 @@
 <template>
-  <v-layout fill-height column justify-space-around class="background">
+  <v-layout fill-height column class="background">
     <v-container xs6>
-      <v-flex text-xs-center xs>
+      <v-layout column fill-height justify-space-around text-xs-center xs>
         <v-flex text-xs-center>
           <h1 class="display-2 font-weight-bold mb-3 white--text">
             Recognize my cat
           </h1>
         </v-flex>
-        <v-spacer></v-spacer>
 
-        <v-flex offset-xs4 xs4 text-xs-center>
-          <v-text-field
-            dark
-            label="Enter your pseudo"
-            outline
-            single-line
-          ></v-text-field>
+        <v-flex>
+          <v-flex offset-xs4 xs4 text-xs-center>
+            <v-text-field
+              dark
+              label="Enter your pseudo"
+              outline
+              single-line
+            ></v-text-field>
+          </v-flex>
+          <v-spacer></v-spacer>
+
+          <v-carousel
+            class="elevation-0"
+            width="50"
+            height="100"
+            xs6
+            hide-delimiters
+            :cycle="false"
+          >
+            <v-carousel-item :key="i" v-for="i in getNumberPagesPictures()">
+              <v-layout row>
+                <v-flex xs4 :key="j" v-for="(item, j) in getPictures(i - 1)">
+                  <v-avatar :size="100" color="grey lighten-4">
+                    <img :src="item.src" alt />
+                  </v-avatar>
+                </v-flex>
+              </v-layout>
+            </v-carousel-item>
+          </v-carousel>
         </v-flex>
-        <v-spacer></v-spacer>
 
-        <v-carousel
-          class="elevation-0"
-          width="50"
-          height="100"
-          xs6
-          hide-delimiters
-          :cycle="false"
-        >
-          <v-carousel-item :key="i" v-for="i in getNumberPagesPictures()">
-            <v-layout row>
-              <v-flex xs4 :key="j" v-for="(item, j) in getPictures(i - 1)">
-                <v-avatar :size="100" color="grey lighten-4">
-                  <img :src="item.src" alt />
-                </v-avatar>
-              </v-flex>
-            </v-layout>
-          </v-carousel-item>
-        </v-carousel>
-      </v-flex>
+        <div class="text-xs-center">
+          <v-btn class="white--text" color="green">Start !</v-btn>
+        </div>
+      </v-layout>
     </v-container>
-    <div class="text-xs-center">
-      <v-btn class="white--text" color="green">Start !</v-btn>
-    </div>
   </v-layout>
 </template>
 
