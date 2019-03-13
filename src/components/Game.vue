66810a918ac1bb4a50d5b93ab1bc1868a2ae29ff
namileto <gregoire.monet@gmail.com>
2019-03-13 11:44:08 +0100

    Added inputs and buttons to Game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,26 +2,27 @@
   <div class="page">
     <v-container>
       <v-layout row class="display-1" text-md-center wrap>
-        <v-flex class="part2" justify-center order-md1 order-xs2 md3>Salut je suis un test</v-flex>
-        <v-flex class="part1" justify-center order-md1 md9>
-            
-            Guess the key word of the image:
+        <v-flex class="part2" justify-center order-md1 order-xs2 md3>Profil joueur</v-flex>
+        <v-flex class="part1" justify-center order-md1 md9>Guess the key word of the image:
+          <v-layout row class="display-1" text-md-center>
+            <v-flex xs8 offset-xs2>
+              <v-layout column>
+                <v-img max-height="800px" v-bind:src="image" aspect-ratio="1.7"></v-img>
+              </v-layout>
+            </v-flex>
+          </v-layout>
 
-        <v-layout row class="display-1" text-md-center>
-          <v-flex xs8 offset-xs2>
-            <v-layout column>
-              <v-img max-height="800px" src="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" aspect-ratio="1.7"></v-img>
-            </v-layout>
-          </v-flex>
-        </v-layout>
+          <v-layout row class="display-1" text-md-center>
+            <v-flex justify-center xs8 offset-xs2>
+              <v-text-field label="Solo" placeholder="Your word" solo></v-text-field>
+            </v-flex>
+          </v-layout>
 
-        <v-layout row class="display-1" text-md-center>
-          <v-flex justify-center xs8 offset-xs2>
-            <v-text-field label="Solo" placeholder="Your word" solo></v-text-field>
-        </v-flex>
-        </v-layout>
-
-
+          <v-layout row class="display-1" text-md-center>
+            <v-flex justify-center xs4 offset-xs4>
+              <v-btn color="#b99458" large>Validate</v-btn>
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
@@ -31,11 +32,13 @@
 <script>
 export default {
   data: () => {
-    return {};
+    return {
+      image:
+        "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"
+    };
   }
 };
 </script>
-
 
 
 <style scopped>
@@ -52,6 +55,8 @@ export default {
 .part2 {
   background-color: red;
 }
+
+
 </style>
 
 
