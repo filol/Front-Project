689c39619a3b042ffe7b475463be562c8c71889b
François D <fdex24@gmail.com>
2019-03-14 19:49:43 +0100

    Fix badge bug

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,56 +3,42 @@
     <v-container xs6>
       <v-layout column fill-height justify-space-around text-xs-center xs>
         <v-flex text-xs-center>
-          <h1 class="display-2 font-weight-bold mb-3 white--text">
-            Recognize my cat
-          </h1>
+          <h1 class="display-2 font-weight-bold mb-3 white--text">Recognize my cat</h1>
         </v-flex>
 
         <v-flex>
           <v-flex offset-xs4 xs4 text-xs-center>
-            <v-text-field
-              dark
-              label="Enter your pseudo"
-              outline
-              single-line
-            ></v-text-field>
+            <v-text-field dark label="Enter your pseudo" outline single-line></v-text-field>
           </v-flex>
           <v-spacer></v-spacer>
+
+          <v-flex text-xs-center>
+            <h2 class="display-6 font-weight-bold mb-3 white--text">Choose your avatar</h2>
+          </v-flex>
 
           <v-carousel
             class="elevation-0"
             width="50"
-            height="100"
+            height="125"
             xs6
             hide-delimiters
             :cycle="false"
           >
-            <v-carousel-item :key="i" v-for="i in getNumberPagesPictures()">
-              <v-layout row>
-                <v-flex xs4 :key="j" v-for="(item, j) in getPictures(i - 1)">
-                  <v-badge
-                    overlap
-                    v-if="selectedAvatar == i * numberAvatarByPage + j"
-                  >
+            <v-carousel-item :key="i" v-for="i in getNumberPagesPicturesArray()">
+              <v-layout row class="pt-2">
+                <v-flex xs4 :key="j" v-for="(item, j) in getPictures(i)">
+                  <v-badge overlap v-if="selectedAvatar == i * numberAvatarByPage + j">
                     <template v-slot:badge>
                       <v-icon>done</v-icon>
                     </template>
-                    <v-avatar
-                      @click="selectAvatar(i, j)"
-                      :size="100"
-                      color="grey lighten-4"
-                    >
-                      <img :src="item.src" alt />
+                    <v-avatar @click="selectAvatar(i, j)" :size="100" color="grey lighten-4">
+                      <img :src="item.src" alt>
                     </v-avatar>
                   </v-badge>
 
                   <v-badge overlap v-else>
-                    <v-avatar
-                      @click="selectAvatar(i, j)"
-                      :size="100"
-                      color="grey lighten-4"
-                    >
-                      <img :src="item.src" alt />
+                    <v-avatar @click="selectAvatar(i, j)" :size="100" color="grey lighten-4">
+                      <img :src="item.src" alt>
                     </v-avatar>
                   </v-badge>
                 </v-flex>
@@ -109,6 +95,13 @@ export default {
     },
     getNumberPagesPictures () {
       return Math.ceil(this.items.length / this.numberAvatarByPage)
+    },
+    getNumberPagesPicturesArray () {
+      var array = []
+      for (var i = 0; i < this.getNumberPagesPictures(); i++) {
+        array.push(i)
+      }
+      return array
     },
     selectAvatar (indexPage, index) {
       this.selectedAvatar = indexPage * this.numberAvatarByPage + index
